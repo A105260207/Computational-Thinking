c4467062d9f356aa1ad66236e51c5d20e00cc877
A105260207 <72373249+A105260207@users.noreply.github.com>
2020-12-28 14:31:15 +0800

    week16

---
+++ week16 A105260207.py
@@ -0,0 +1,38 @@
+print(True)
+print(False)
+
+print(bool(1))
+print(bool(0))
+print(bool(()))
+
+print(False+1)
+
+score = 66
+score == 66
+
+name = "Francis"
+name == "Francis"
+name = "Francis"
+
+5>2
+5<2
+5 !=2
+
+True&True
+True&False
+False&True
+False&False
+True and False
+True or False
+False or True
+False| False
+not True
+not False
+
+X = 70
+print((X>60) and (X<80))
+print((x >60) or (x<80))
+print(not(x > 60))
+
+"H" in "Hello world"
+"h" in "Hello world"
